bb9ed5efd5990acbe24f5b511d4a82d8a17ebd33
vasuvibingprivetly <bhurakhyavasu@gmail.com>
2024-07-02 10:31:29 +0530

    02/07/2024

--- lib/views/HomePage.dart
+++ lib/views/HomePage.dart
@@ -1,5 +1,7 @@
 import 'package:e_commerce_app/utils/product_utils.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,7 +15,18 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext Context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blue,
         title: const Text("Home Page"),
+        actions: const [
+          Icon(Icons.search),
+          SizedBox(
+            width: 10,
+          ),
+          Icon(CupertinoIcons.cart),
+          SizedBox(
+            width: 10,
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
@@ -24,43 +37,98 @@ class _HomePageState extends State<HomePage> {
               child: Column(
                 children: [
                   Container(
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        children: allProduct
-                            .map(
-                              (e) => Container(
-                                height: 200,
-                                width: 150,
-                                margin: const EdgeInsets.only(
-                                  right: 10,
-                                  bottom: 10,
-                                ),
-                                padding: const EdgeInsets.all(10),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  color: Colors.grey.shade200,
-                                  boxShadow: const [
-                                    BoxShadow(
-                                      color: Colors.grey,
-                                      offset: Offset(5, 5),
-                                      blurRadius: 10,
-                                    ),
-                                  ],
-                                ),
-                                child: Text(
-                                  e['title'],
-                                  maxLines: 1,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: const TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 24,
-                                  ),
-                                ),
-                              ),
-                            )
-                            .toList(),
+                    height: 200,
+                    decoration: const BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(
+                          10,
+                        ),
                       ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: allProduct
+                          .map(
+                            (e) => Container(
+                              height: 250,
+                              width: 170,
+                              margin: const EdgeInsets.only(
+                                right: 10,
+                                bottom: 10,
+                              ),
+                              padding: const EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.grey.shade200,
+                                boxShadow: const [
+                                  BoxShadow(
+                                    color: Colors.grey,
+                                    offset: Offset(5, 5),
+                                    blurRadius: 10,
+                                  ),
+                                ],
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Expanded(
+                                      child: Image(
+                                    image: NetworkImage(
+                                      e['thumbnail'],
+                                    ),
+                                  )),
+                                  const SizedBox(
+                                    height: 10,
+                                  ),
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          maxLines: 1,
+                                          e['title'],
+                                          style: const TextStyle(
+                                              fontSize: 24,
+                                              fontWeight: FontWeight.bold),
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                        Text(
+                                          "\$ ${e['price']}",
+                                          style: const TextStyle(
+                                            fontSize: 20,
+                                          ),
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Align(
+                                    alignment: Alignment.bottomRight,
+                                    child: RatingBar.builder(
+                                      allowHalfRating: true,
+                                      initialRating: e['rating'].toDouble(),
+                                      itemCount: 5,
+                                      itemSize: 22,
+                                      itemBuilder: (context, _) => const Icon(
+                                        Icons.star,
+                                        color: Colors.amber,
+                                      ),
+                                      onRatingUpdate: (rating) {},
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          )
+                          .toList(),
                     ),
                   ),
                 ],
