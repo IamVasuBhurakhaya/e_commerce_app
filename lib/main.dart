12db4472424f9272c3e7859d3d06ff1ea46e985c
vasuvibingprivetly <bhurakhyavasu@gmail.com>
2024-07-10 12:56:40 +0530

    10/07/2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
-import 'package:e_commerce_app/views/HomePage.dart';
+import 'package:e_commerce_app/views/cart_page.dart';
+import 'package:e_commerce_app/views/detail_page.dart';
+import 'package:e_commerce_app/views/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Myapp(),
+    const Myapp(),
   );
 }
 
@@ -11,10 +13,14 @@ class Myapp extends StatelessWidget {
   const Myapp({super.key});
 
   @override
-  Widget build(BuildContext Context) {
-    return const MaterialApp(
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      routes: {
+        '/': (context) => const HomePage(),
+        'detail_page': (context) => const DetailPage(),
+        'cart_page': (context) => const CartPage(),
+      },
     );
   }
 }
